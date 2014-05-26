719b97778efe5bdd530b1ac040db068528ac46a3
Young Hahn <young@mapbox.com>
2014-05-25 22:09:16 -0400

    Remove stale methods from testsource.

--- test/testsource.js
+++ test/testsource.js
@@ -15,72 +15,13 @@ Testsource.now = new Date;
 Testsource.tiles = tiles;
 Testsource.grids = grids;
 
-var search = {
-    'seattle': [
-        {
-            text: 'seattle-,seattle bar',
-            id: '138155',
-            zxy: [ '11/323/1287' ]
-        },
-        {
-            text: 'seattle',
-            id: '219339',
-            zxy: [
-                '11/327/1331',
-                '11/327/1332',
-                '11/327/1333',
-                '11/328/1331',
-                '11/328/1332',
-                '11/328/1333'
-            ]
-        }
-    ],
-    '219339': [
-        {
-            text: 'seattle',
-            id: '219339',
-            zxy: [
-                '11/327/1331',
-                '11/327/1332',
-                '11/327/1333',
-                '11/328/1331',
-                '11/328/1332',
-                '11/328/1333'
-            ]
-        }
-    ]
-};
-var feature = {
-    '219339.raw': {
-        _terms: [ '/mapbox-places/term/seattle.219339.11,327,1331.11,327,1332.11,327,1333.11,328,1331.11,328,1332.11,328,1333' ],
-        bounds: '-122.459696,47.4817199999999,-122.224433,47.734145',
-        lat: 47.6204993,
-        lon: -122.3508761,
-        name: 'Seattle',
-        score: 900000369465466.8,
-        search: 'Seattle',
-        type: 'city'
-    },
-    '219339': {
-        bounds: '-122.459696,47.4817199999999,-122.224433,47.734145',
-        lat: 47.6204993,
-        lon: -122.3508761,
-        name: 'Seattle',
-        score: 900000369465466.8,
-        search: 'Seattle',
-        type: 'city'
-    }
-};
-
 // Define a mock test source.
 function Testsource(uri, callback) {
     this._uri = uri;
     this.hostname = uri.hostname || 'test';
     this.data = { _carmen: 'http://www.example.com' };
     this.stat = {
-        'get': 0,
-        'search': 0,
-        'feature': 0
+        'get': 0
     };
     callback(null, this);
 };
@@ -155,18 +96,4 @@ Testsource.prototype.getGrid = function(z, x, y, callback) {
         return callback(null, JSON.parse(buffer), headers);
     });
 };
-Testsource.prototype.search = function(query, id, callback) {
-    this.stat.search++;
-    if (query === 'seattle') return callback(null, search['seattle'].slice(0));
-    if (id === '219339') return callback(null, search['219339'].slice(0));
-    return callback(null, [].slice(0));
-};
-Testsource.prototype.feature = function(id, callback, raw) {
-    this.stat.feature++;
-    if (id !== '219339') return callback(new Error('Not found'));
-    if (raw) return callback(null, feature['219339.raw']);
-    if (!raw) return callback(null, feature['219339']);
-    return callback(new Error('Not found'));
-};
-
 
